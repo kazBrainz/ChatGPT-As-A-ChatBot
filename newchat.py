b92d1d95a4aaed69cc58a221417e967d48a44268
Kazim Olaniran <96773276+kazBrainz@users.noreply.github.com>
2023-08-27 08:01:14 +0100

    Add files via upload

---
+++ newchat.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "(Your-API-key Here"
+
+model_engine = "text-davinci-003"
+prompt = str(input())
+
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5
+)
+
+response = completion.choices[0].text
+print(response)
